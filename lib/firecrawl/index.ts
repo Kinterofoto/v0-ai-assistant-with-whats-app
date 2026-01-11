c511847150db2dbe85e9d1fa74d5fc31d33c5b9e
Jibaru <ignacioruedaboada@hotmail.com>
2026-01-11 16:14:27 -0500

    feat: add scrap

---
+++ lib/firecrawl/index.ts
@@ -0,0 +1,17 @@
+import Firecrawl from '@mendable/firecrawl-js';
+
+const firecrawl = new Firecrawl({ apiKey: process.env.FIRECRAWL_API_KEY });
+
+
+export async function scrape(input: string): Promise<string> {
+  const response = await firecrawl.scrape(`https://listado.mercadolibre.com.pe/${input}`, {
+      onlyMainContent: true,
+      maxAge: 172800000,
+      proxy: 'stealth',
+      parsers: ['pdf'],
+      formats: ['markdown'],
+      waitFor: 3000
+    });
+
+  return response.markdown!;
+}
---
+++ lib/openai/index.ts
@@ -0,0 +1,36 @@
+import { Output, generateText } from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { z } from 'zod';
+
+export interface Product {
+    name: string;
+    url: string;
+}
+
+const productSchema = z.object({
+    products: z.array(
+        z.object({
+            name: z.string().describe('The name or title of the product'),
+            url: z.string().describe('The URL link to the product page')
+        })
+    )
+});
+
+export async function extractContent(data: string, keyword: string): Promise<Product[]> {
+    //console.log(data);
+    
+    const { output } = await generateText({
+        model: openai('gpt-4o'),
+        
+        prompt: `Extract the top 10 related products with their URLs from the following content related to the keyword: ${keyword}.
+        For each product, provide its name and the full URL in a list.
+
+        Content:
+        ${data}`,
+        output:  Output.object({
+            schema: productSchema,
+          }),
+    });
+
+    return output.products;
+}
